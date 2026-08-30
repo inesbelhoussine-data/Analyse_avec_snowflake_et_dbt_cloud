bb37eb4fb0446ddf934628fe2c2eb7f7f8d3a2d2
Ines <129823665+inesbelhoussine-data@users.noreply.github.com>
2026-08-30 23:44:36 +0200

    Create int_student_clean model for data preparation
    This SQL model cleans and prepares student data by removing duplicates, retaining useful columns, and preparing the data for the final mart.

--- Models/intermediate/int_student_clean.sql
+++ Models/intermediate/int_student_clean.sql
@@ -1,1 +1,41 @@
+-- Modèle : int_students_clean
+-- Objectif : nettoyer et préparer les données étudiants.
+-- Transformations réalisées :
+-- - suppression des doublons (1 étudiant par année)
+-- - conservation des colonnes utiles
+-- - préparation des données pour le mart final.
+with students as (
 
+    select *
+    from {{ ref('stg_raw__STUDENTS') }}
+
+),
+
+deduplicated as (
+
+    select
+        user_id,
+        path_category_name,
+        age_group,
+        gender,
+        region,
+        year_path_started,
+
+        row_number() over (
+             partition by user_id, year_path_started
+            order by user_id
+        ) as row_num
+
+    from students
+
+)
+
+select
+    user_id,
+    path_category_name,
+    age_group,
+    gender,
+    region,
+    year_path_started
+from deduplicated
+where row_num = 1
